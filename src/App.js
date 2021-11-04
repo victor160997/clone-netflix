dfed5f2b8aa824c7e3a45981b8453204c756736a
Victor Mendonca <victor.men.dev@gmail.com>
2021-11-04 16:34:59 -0300

    criada funçoes que fazendo as requisiçoes a API themoviedb

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { getInfos } from './services/requestTmdb';
 
 function App() {
+
+  useEffect(() => {
+    const getLists = async () => {
+      const list = await getInfos();
+      console.log(list);
+    }
+
+    getLists();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hellowwwww
     </div>
   );
 }
---
+++ src/services/requestTmdb.js
@@ -0,0 +1,68 @@
+const urlBase = 'https://api.themoviedb.org/3';
+const languageAndKey = '&api_key=e70501c774a05cf85580e32a27897dc6&language=pt-BR';
+
+/* 
+- originais netflix
+- recomendados
+- em alta
+- ação
+- comédia
+- terror
+- romance
+- documentários
+*/
+
+async function fetchDataBase(endpoint) {
+  try {
+    const response = await fetch(`${urlBase}${endpoint}${languageAndKey}`);
+    const json = await response.json();
+    return json;
+  } catch {
+    console.log('Erro')
+  }
+}
+
+export const getInfos = async () => {
+  return [
+    {
+      category: 'originals',
+      title: 'Originais da Netflix',
+      items: await fetchDataBase('/discover/tv?with_network=213')
+    },
+    {
+      category: 'trending',
+      title: 'Recomendados',
+      items: await fetchDataBase('/trending/all/week?')
+    },
+    {
+      category: 'toprated',
+      title: 'Em alta',
+      items: await fetchDataBase('/movie/top_rated?')
+    },
+    {
+      category: 'action',
+      title: 'Ação',
+      items: await fetchDataBase('/discover/movie?with_genres=28')
+    },
+    {
+      category: 'comedy',
+      title: 'Comédia',
+      items: await fetchDataBase('/discover/movie?with_genres=35')
+    },
+    {
+      category: 'horror',
+      title: 'Terror',
+      items: await fetchDataBase('/discover/movie?with_genres=27')
+    },
+    {
+      category: 'romance',
+      title: 'Romance',
+      items: await fetchDataBase('/discover/movie?with_genres=10749')
+    },
+    {
+      category: 'documentary',
+      title: 'Documentários',
+      items: await fetchDataBase('/discover/movie?with_genres=99')
+    }
+  ];
+}
